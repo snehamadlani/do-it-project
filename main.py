edd1b86d9a1c1afb59ebf0aeddae9bfad0314ff1
Sneha Madlani <sneha.madlani@gmail.com>
2021-12-10 18:08:37 +0000

    key value retrieve change

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ db.create_all()
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    all_books = db.session.query(Book).all()
+    return render_template("index.html", books=all_books)
 
 
 @app.route("/add", methods=["GET", "POST"])
@@ -42,10 +43,17 @@ def add():
     return render_template("add.html")
 
 
-@app.route('/retrieve')
+@app.route('/retrieve', methods=['GET', 'POST'])
 def retrieve():
-    all_books = db.session.query(Book).all()
-    return render_template("retrieve.html", books=all_books)
+    if request.method == 'POST':
+        book_id = request.form["id"]
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = Book.query.get(book_id)
+    return render_template("retrieve.html", book=book_selected)
 
 
 if __name__ == "__main__":

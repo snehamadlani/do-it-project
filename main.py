d3e1ef7212f7fb4e44cc91bdd08f11915843ec42
Sneha Madlani <sneha.madlani@gmail.com>
2021-12-13 23:32:04 +0000

    flask login authentication

--- main.py
+++ main.py
@@ -1,32 +1,94 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, send_from_directory
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from functools import wraps
+from flask import abort
 
 app = Flask(__name__)
+Bootstrap(app)
 
 # CREATE DATABASE
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
 # silence the deprecation warning in the console.
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-# CREATE TABLE
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# CREATE REGISTRATION FORM
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Sign Me Up!")
+
+
+# CREATE LOGIN FORM
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Let Me In!")
+
+# Create Book Table
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
     rating = db.Column(db.Float, nullable=False)
 
+#Create the User Table
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
 
 db.create_all()
 
 
+# Create admin-only decorator
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # If email is not admin@email.com then return abort with 403 error
+        if current_user.email != "admin@email.com":
+            return abort(403)
+        # Otherwise continue with the route function
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 @app.route('/')
 def home():
+    return render_template("index.html")
+
+
+@app.route('/home')
+def home2():
+    return render_template("home.html", logged_in=current_user.is_authenticated)
+
+
+@app.route('/books')
+def books():
     all_books = db.session.query(Book).all()
-    return render_template("index.html", books=all_books)
+    return render_template("books.html", books=all_books, logged_in=current_user.is_authenticated)
 
 
 @app.route("/add", methods=["GET", "POST"])
+# Mark with decorator
+@admin_only
 def add():
     if request.method == "POST":
         # CREATE RECORD
@@ -40,20 +102,82 @@ def add():
         # redirect to the home page after the form has been submitted.
         return redirect(url_for('home'))
 
-    return render_template("add.html")
+    return render_template("add.html", logged_in=current_user.is_authenticated)
 
 
-@app.route('/retrieve', methods=['GET', 'POST'])
-def retrieve():
+@app.route('/edit', methods=['GET', 'POST'])
+# Mark with decorator
+@admin_only
+def edit():
     if request.method == 'POST':
         book_id = request.form["id"]
         book_to_update = Book.query.get(book_id)
         book_to_update.rating = request.form["rating"]
         db.session.commit()
-        return redirect(url_for('home'))
+        return redirect(url_for('books'))
     book_id = request.args.get('id')
     book_selected = Book.query.get(book_id)
-    return render_template("retrieve.html", book=book_selected)
+    return render_template("edit.html", book=book_selected, logged_in=current_user.is_authenticated)
+
+
+# Register new users into the User database
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+
+        # If user's email already exists
+        if User.query.filter_by(email=form.email.data).first():
+            # Send flash messsage
+            flash("You've already signed up with that email, log in instead!")
+            # Redirect to /login route.
+            return redirect(url_for('login'))
+
+        hash_and_salted_password = generate_password_hash(
+            form.password.data,
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        new_user = User(
+            email=form.email.data,
+            name=form.name.data,
+            password=hash_and_salted_password,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for("home"))
+
+    return render_template("register.html", form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        user = User.query.filter_by(email=email).first()
+        # Email doesn't exist
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        # Password incorrect
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('home'))
+    return render_template("login.html", form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
